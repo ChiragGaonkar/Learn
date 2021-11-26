9ef6d53b02b61bac299144e90917a7cdb5541d7f
Chirag Gaonkar <chiraggaonkar80@gmail.com>
2021-11-26 22:51:09 +0530

    Modified Config.php

--- config.php
+++ config.php
@@ -9,7 +9,7 @@ $conn = new mysqli($server, $user, $pass);
 if (!$conn) {
     die("<script>alert('Connection Failed.')</script>");
 } else {
-    echo "<script>alert('Connection Established.')</script>";
+    echo "<script>alert('Connection Established Successfully.')</script>";
     $sql = "CREATE DATABASE learn";
     $conn->query($sql);
     mysqli_select_db($conn, 'learn');
