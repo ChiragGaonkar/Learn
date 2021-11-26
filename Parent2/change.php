6031fac40b3809e950911aef345d2fe67b085edd
Chirag Gaonkar <chiraggaonkar80@gmail.com>
2021-11-27 00:36:08 +0530

    Parent 2

---
+++ Parent2/change.php
@@ -0,0 +1,27 @@
+<?php
+include 'config.php';
+$sql = "Alter table EMPLOYEE rename column EPHONE to EmployeePhoneNumber";
+$RESULT = $conn->query($sql);
+if ($RESULT) {
+    echo "<script>alert('Done')</script>";
+} else {
+    echo "<script>alert('Error')</script>";
+}
+
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Welcome</title>
+</head>
+
+<body>
+
+</body>
+
+</html>
